2687562ed9e1a6263f7c6af4ce709fb4a3a52ea9
liujie <482866003@qq.com>
2019-11-27 12:16:41 +0800

    xiugai

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-const obj = {
-    userName:"laotie"
-}
-function fn() {
-    a = 12;1
-    console.log(this.userName,a);
-}
-// fn.call(obj);
-// console.log(a);
-// fn.bind(obj)();
-const myFn = fn.bind(obj);
-myFn();
+cqweqqqqqqqqqqq
